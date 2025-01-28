276152fec600a295ab28cd4314ef9e202e55b678
Benjamin Edlinger <bedlinger@student.tgm.ac.at>
2025-01-28 23:18:56 +0100

    edit alt text of images

--- pages/showcase.vue
+++ pages/showcase.vue
@@ -7,7 +7,7 @@
     </p>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div v-for="(image, index) in images" :key="index">
-        <Image :src="image" alt="Image" preview />
+        <Image :src="image" :alt="'Screenshot ' + index" preview />
       </div>
     </div>
   </div>
